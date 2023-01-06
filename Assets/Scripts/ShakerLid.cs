9487946662371389097d14845e77269056d74f96
Ayamohamedaya <50969015+Ayamohamedaya@users.noreply.github.com>
2023-01-06 12:50:42 +0200

    shake animation

--- Assets/Scripts/ShakerLid.cs
+++ Assets/Scripts/ShakerLid.cs
@@ -10,18 +10,23 @@ public class ShakerLid : Holder
     {
         _rb.isKinematic = true;
         shaker.GetComponent<Rigidbody>().isKinematic = true;
-       // UIManager.instance.shakeButton.SetActive(false);
-
-        transform.parent = shaker.transform;
-        transform.DOMove(shakerClosePosition.position,1);
+        // UIManager.instance.shakeButton.SetActive(false);
+        shaker.GetComponent<Shaker>().Shake();
         AudioManagerMain.instance.PlaySFX("shakerSound");
-        transform.DORotate(shakerClosePosition.rotation.eulerAngles, 1).OnComplete(() =>
-        shaker.transform.DOShakePosition(9.5f,1).OnComplete(() => FinishShake()));
+        GetComponent<BoxCollider>().enabled = false;
+        gameObject.SetActive(false);
+
+      //  transform.parent = shaker.transform;
+       // transform.DOMove(shakerClosePosition.position,1);
+       // transform.DORotate(shakerClosePosition.rotation.eulerAngles, 1).OnComplete(() =>
     }
     public void FinishShake()
     {
+       shaker.GetComponent<Shaker>().dummyLid.SetActive(false);
+        gameObject.SetActive(true);
+
         AudioManagerMain.instance.PlaySFX("shakerMixerOpen");
-        transform.parent = transform.parent.parent;
+      //  transform.parent = transform.parent.parent;
         // shaker.GetComponent<Rigidbody>().isKinematic = false;
         SceneController.instance.InvokeCurrentStep();
 
